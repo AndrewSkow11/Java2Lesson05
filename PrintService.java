e2c4ff2e6ec470fbb293cedf9b01e900c30f4cd2
Andrew Skow <skovorodnikov11@gmail.com>
2024-10-14 15:26:46 +0300

    ---

--- PrintService.java
+++ PrintService.java
@@ -1,21 +1,7 @@
-public class PrintService {
-    public void print(Person [] people) {
-        for (Person person : people) {
-            System.out.println(person);
-        }
-    }
+public interface PrintService {
+    void print(Person[] people);
 
+    void print(Customer[] customers);
 
-    public void print(Customer [] customers) {
-        for (Customer customer : customers) {
-            System.out.println(customer);
-        }
-    }
-
-    public void print(Gamer [] gamers) {
-        for (Person gamer : gamers) {
-            System.out.println(gamer);
-        }
-    }
-
+    void print(Gamer[] gamers);
 }
---
+++ PrintServiceForInvestor.java
@@ -0,0 +1,40 @@
+
+
+public class PrintServiceForInvestor implements PrintService {
+    @Override
+    public void print(Person[] people) {
+        System.out.println("\nРаспечатка Person");
+
+        for (Person person : people) {
+            System.out.println(person);
+        }
+
+        System.out.println();
+    }
+
+
+    @Override
+    public void print(Customer[] customers) {
+        System.out.println("\nРаспечатка Customer");
+
+        for (Customer customer : customers) {
+            System.out.println(customer);
+        }
+
+        System.out.println();
+
+    }
+
+    @Override
+    public void print(Gamer[] gamers) {
+        System.out.println("\nРаспечатка Gamer");
+
+        for (Person gamer : gamers) {
+            System.out.println(gamer);
+        }
+
+        System.out.println();
+
+    }
+
+}
